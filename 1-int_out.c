4fbb41ed09455bdceb0b447c05030b2d61f9efba
qwab17 <quadriwasiubiodun@gmail.com>
2023-09-17 06:01:52 -0700

    task 1 update

--- 1-int_out.c
+++ 1-int_out.c
@@ -1,41 +1,41 @@
 #include "main.h"
 
 /**
- * print_int - function that Prints int.
- * @a: The va_list containing int.
+ * print_int - function that Prints integer.
+ * @a: The va_list containing integer.
  * Return: The number of characters printed.
  */
 
 int print_int(va_list a)
 {
-	int num = va_arg(a, int);
+	int nbr = va_arg(a, int);
 	int len = 0;
 
     /* Print the integer */
-	if (num < 0)
+	if (nbr < 0)
 	{
 		_putchar('-');
-		num = -num;
+		nbr = -nbr;
 		len++;
 	}
-	if (num == 0)
+	if (nbr == 0)
 	{
 		_putchar('0');
 		len++;
 	}
 	else
 	{
-		int r_num = 0;
+		int r_nbr = 0;
 
-		while (num > 0)
+		while (nbr > 0)
 		{
-			r_num = (r_num * 10) + (num % 10);
-			num /= 10;
+			r_nbr = (r_nbr * 10) + (nbr % 10);
+			nbr /= 10;
 		}
-		while (r_num > 0)
+		while (r_nbr > 0)
 		{
-			_putchar((r_num % 10) + '0');
-			r_num /= 10;
+			_putchar((r_nbr % 10) + '0');
+			r_nbr /= 10;
 			len++;
 		}
 	}
