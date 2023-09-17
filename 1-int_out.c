9e9a672210b54df75bcb379561d5b8fe377f66b6
ASLawan <nawalnitsua@gmail.com>
2023-09-17 11:53:28 +0100

    Update: print correct output(integer)

--- 1-int_out.c
+++ 1-int_out.c
@@ -1,18 +1,43 @@
 #include "main.h"
 
 /**
- * print_integer - function that Prints int.
+ * print_int - function that Prints int.
  * @a: The va_list containing int.
  * Return: The number of characters printed.
  */
 
-int print_integer(va_list a)
+int print_int(va_list a)
 {
-    int num = va_arg(a, int);
-    int len = 0;
+	int num = va_arg(a, int);
+	int len = 0;
 
     /* Print the integer */
-    len += printf("%d", num);
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		len++;
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+		len++;
+	}
+	else
+	{
+		int r_num = 0;
 
-    return (len);
+		while (num > 0)
+		{
+			r_num = (r_num * 10) + (num % 10);
+			num /= 10;
+		}
+		while (r_num > 0)
+		{
+			_putchar((r_num % 10) + '0');
+			r_num /= 10;
+			len++;
+		}
+	}
+	return (len);
 }
