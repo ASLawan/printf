369a197bbbb59eaa8071e018ecf00929a7600c31
ASLawan <nawalnitsua@gmail.com>
2023-09-18 02:55:01 +0100

    Change function argument

--- print_percent.c
+++ print_percent.c
@@ -2,11 +2,15 @@
 
 /**
  * print_percent - prints percent sign
+ * @a: unnamed arguments
  * Return: always 0
  */
 
-int print_percent(void)
+int print_percent(va_list a)
 {
-	_putchar('%');
+	int c = va_arg(a, int);
+
+	if (c)
+		_putchar('%');
 	return (1);
 }
